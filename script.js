0b85c309e2fe3623dd0539c2be687ee2f587ca45
Smudgy <tmrgast@live.nl>
2017-10-16 15:19:10 +0200

    -

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Temperature sliders
 var sliderDay = document.getElementById("myDay");
 var outputDay = document.getElementById("day");
 outputDay.innerHTML = sliderDay.value;
@@ -22,4 +23,35 @@ sliderOverride.oninput = function() {
     outputOverride.innerHTML = this.value;
 }
 
+// Weekly schedule
 
+var dayNumber = new Date().getDay(); // day of the week in numbers (1-7)
+
+// Days, getting values from schedule.html
+var monDay = document.getElementById("monDay");
+var monNight = document.getElementById("monNight");
+var monSwitch = document.getElementById("thuSwitch");
+
+var tueDay = document.getElementById("tueDay");
+var tueNight = document.getElementById("tueNight");
+var tueSwitch = document.getElementById("thuSwitch");
+
+var wedDay = document.getElementById("wedDay");
+var wedNight = document.getElementById("wedNight");
+var wedSwitch = document.getElementById("thuSwitch");
+
+var thuDay = document.getElementById("thuDay");
+var thuNight = document.getElementById("thuNight");
+var thuSwitch = document.getElementById("thuSwitch");
+
+var friDay = document.getElementById("friDay");
+var friNight = document.getElementById("friNight");
+var friSwitch = document.getElementById("thuSwitch");
+
+var satDay = document.getElementById("satDay");
+var satNight = document.getElementById("satNight");
+var satSwitch = document.getElementById("thuSwitch");
+
+var sunDay = document.getElementById("sunDay");
+var sunNight = document.getElementById("sunNight");
+var sunSwitch = document.getElementById("thuSwitch");
