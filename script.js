597f975cf5f337e50162e4b66be46debfb381c9b
Smudgy <tmrgast@live.nl>
2017-10-18 20:13:49 +0200

    -

--- script.js
+++ script.js
@@ -27,31 +27,33 @@ sliderOverride.oninput = function() {
 
 var dayNumber = new Date().getDay(); // day of the week in numbers (1-7)
 
+
+
 // Days, getting values from schedule.html
-var monDay = document.getElementById("monDay");
-var monNight = document.getElementById("monNight");
-var monSwitch = document.getElementById("thuSwitch");
+var monDay = document.getElementById("monDay").value;
+var monNight = document.getElementById("monNight").value;
+var monSwitch = document.getElementById("thuSwitch").value;
 
-var tueDay = document.getElementById("tueDay");
-var tueNight = document.getElementById("tueNight");
-var tueSwitch = document.getElementById("thuSwitch");
+var tueDay = document.getElementById("tueDay").value;
+var tueNight = document.getElementById("tueNight").value;
+var tueSwitch = document.getElementById("thuSwitch").value;
 
-var wedDay = document.getElementById("wedDay");
-var wedNight = document.getElementById("wedNight");
-var wedSwitch = document.getElementById("thuSwitch");
+var wedDay = document.getElementById("wedDay").value;
+var wedNight = document.getElementById("wedNight").value;
+var wedSwitch = document.getElementById("thuSwitch").value;
 
-var thuDay = document.getElementById("thuDay");
-var thuNight = document.getElementById("thuNight");
-var thuSwitch = document.getElementById("thuSwitch");
+var thuDay = document.getElementById("thuDay").value;
+var thuNight = document.getElementById("thuNight").value;
+var thuSwitch = document.getElementById("thuSwitch").value;
 
-var friDay = document.getElementById("friDay");
-var friNight = document.getElementById("friNight");
-var friSwitch = document.getElementById("thuSwitch");
+var friDay = document.getElementById("friDay").value;
+var friNight = document.getElementById("friNight").value;
+var friSwitch = document.getElementById("thuSwitch").value;
 
-var satDay = document.getElementById("satDay");
-var satNight = document.getElementById("satNight");
-var satSwitch = document.getElementById("thuSwitch");
+var satDay = document.getElementById("satDay").value;
+var satNight = document.getElementById("satNight").value;
+var satSwitch = document.getElementById("thuSwitch").value;
 
-var sunDay = document.getElementById("sunDay");
-var sunNight = document.getElementById("sunNight");
-var sunSwitch = document.getElementById("thuSwitch");
+var sunDay = document.getElementById("sunDay").value;
+var sunNight = document.getElementById("sunNight").value;
+var sunSwitch = document.getElementById("thuSwitch").value;
